7542e6e50d36ac344b2dbae147fa1d843063aa75
Carina Rodrigues <crodrigues.prog@gmail.com>
2025-10-04 17:46:27 -0300

    Se creó nuevo Componente MemberButton para Sidebar

---
+++ src/components/MemberButton.jsx
@@ -0,0 +1,12 @@
+import '../components/MemberButton.css';
+
+export default function MemberButton({imgsrc, name, onClick}){
+    return(
+        <button className='member-button' onClick={onClick}>
+            <div className='member-img-container'>
+                <img src={imgsrc} alt={name} className='member-img'/>
+            </div>
+            <a className='member-name'>{name}</a>
+        </button>
+    );
+}
